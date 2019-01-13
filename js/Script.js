9a53254c54cbfd484ceacea51c9aa2da0084a296
kalle <kmthorsnes@gmail.com>
2019-01-13 16:25:07 +0100

    updates font

--- js/Script.js
+++ js/Script.js
@@ -1,13 +1,16 @@
 // Declaring global variables
-const clock = document.getElementById("clock");
-
+let clock = document.getElementById("clock");
+let date = new Date();
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
 //Setter klokkeslett
 
 function setTime() {
-  const date = new Date();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
 
   function hourWithLeadingZero() {
     if (hour < 10) {
@@ -47,86 +50,106 @@ setTime();
 //Set clock to update every second
 setInterval(setTime, 1000);
 
-// Adjusting style
 
-// Changing background constructor(props)
-function randomColorNumbers(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// New take on colorGradient-change codebase from: https://hugogiraudel.com/2015/01/12/color-clock-experiment/
 
-// Creates random RGB-color starting point
-
-randomR = randomColorNumbers(0, 255);
-randomG = randomColorNumbers(0, 255);
-randomB = randomColorNumbers(0, 255);
-
-function changeBackgroundforreal() {
-  // Initial functions creating of random numbers from 0-255 for color scale
-
-  // Function for appending colors gradient by 1/255.
-
-  console.log(randomR, randomG, randomB);
-
-  function newRandomPlusOne(e) {
-    if (e !== 255) {
-      let b = e + 1;
-      while (e < b) {
-        e++;
-        return e;
-      }
-    } else {
-      e = 0;
-      return e;
-    }
-  }
-
-  console.log(newRandomPlusOne(randomR));
-}
-
-var i = 12;
-function increment() {
-  i++;
-  document.getElementById("number").innerHTML = i;
-}
-
-function newRandomR() {
-  if (randomR !== 255) {
-    randomR++;
-  } else {
-    randomR = 0;
-  }
-
-}
-function newRandomG() {
-    if (randomG !== 255) {
-      randomG++;
-    } else {
-      randomG = 0;
-    }
-  }
-
-  function newRandomB() {
-    if (randomB !== 255) {
-      randomB++;
-    } else {
-      randomB = 0;
-    }
-  }
-
-  function backgroundChange(){
+function RGBFromDate() {
+    const date = new Date();
+    hour =  date.getHours() / 24 * 255;
+    minute =  date.getMinutes() / 60 * 255;
+    second =  date.getSeconds() / 60 * 255;
+    RGBhour = Math.round(hour);
+    RGBminute = Math.round(minute);
+    RGBsecond = Math.round(second);
+    console.log(RGBhour, RGBminute, RGBsecond);
     document.body.style.backgroundColor =
-    "rgb(" + randomR + "," + randomG + "," + randomB + ")";
-  };
-
-function changeBackground(){
-    newRandomR();
-    newRandomG();
-    newRandomB();
-    backgroundChange();
-    console.log(randomR, randomB, randomG);
+    "rgb(" + RGBhour + "," + RGBminute + "," + RGBsecond + ")"
 }
+    
+setInterval(RGBFromDate, 100);
 
-setInterval(changeBackground, 1000);
 
+
+
+
+// // Adjusting style
+
+// // Changing background constructor(props)
+// function randomColorNumbers(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// // Creates random RGB-color starting point
+
+// randomR = randomColorNumbers(0, 255);
+// randomG = randomColorNumbers(0, 255);
+// randomB = randomColorNumbers(0, 255);
+
+// function changeBackgroundforreal() {
+//   // Initial functions creating of random numbers from 0-255 for color scale
+
+//   // Function for appending colors gradient by 1/255.
+
+//   console.log(randomR, randomG, randomB);
+
+//   function newRandomPlusOne(e) {
+//     if (e !== 255) {
+//       let b = e + 1;
+//       while (e < b) {
+//         e++;
+//         return e;
+//       }
+//     } else {
+//       e = 0;
+//       return e;
+//     }
+//   }
+
+//   console.log(newRandomPlusOne(randomR));
+// }
+
+// var i = 12;
+// function increment() {
+//   i++;
+//   document.getElementById("number").innerHTML = i;
+// }
+
+// function newRandomR() {
+//   if (randomR !== 255) {
+//     randomR++;
+//   } else {
+//     randomR = 0;
+//   }
+// }
+// function newRandomG() {
+//   if (randomG !== 255) {
+//     randomG++;
+//   } else {
+//     randomG = 0;
+//   }
+// }
+
+// function newRandomB() {
+//   if (randomB !== 255) {
+//     randomB++;
+//   } else {
+//     randomB = 0;
+//   }
+// }
+
+// function backgroundChange() {
+//   document.body.style.backgroundColor =
+//     "rgb(" + randomR + "," + randomG + "," + randomB + ")";
+// }
+
+// function changeBackground() {
+//   newRandomR();
+//   newRandomG();
+//   newRandomB();
+//   backgroundChange();
+//   console.log(randomR, randomB, randomG);
+// }
+
+// setInterval(changeBackground, 1000);
